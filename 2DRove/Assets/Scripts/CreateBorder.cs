3d209f6f66af0c1ebebf3089b98881bcba8f14a8
SwiftNemesis <bbsowers@gmail.com>
2024-02-28 01:12:38 -0800

    Update MapGen.cs with new bounds and error handling

---
+++ 2DRove/Assets/Scripts/CreateBorder.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class responsible for creating a border line in a 2D space.
+/// </summary>
+namespace CreateBorder
+{
+    public class DrawBorder : MonoBehaviour
+    {
+        [SerializeField] float lineWidth = 20f;
+        /// <summary>
+        /// Draws a border line using a LineRenderer component.
+        /// </summary>
+        /// <param name="minX">The minimum X coordinate of the border.</param>
+        /// <param name="minY">The minimum Y coordinate of the border.</param>
+        /// <param name="maxX">The maximum X coordinate of the border.</param>
+        /// <param name="maxY">The maximum Y coordinate of the border.</param>
+        public void DrawBorderLine(int minX, int minY, int maxX, int maxY)
+        {
+            //Create a new GameObject and add a LineRenderer component to it
+            GameObject borderLine = new GameObject("BorderLine");
+            //Add a LineRenderer component to the GameObject
+            LineRenderer lineRenderer = borderLine.AddComponent<LineRenderer>();
+
+            //Assign the default material for linerender
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            
+            //Set the number of positions in the LineRenderer to 5
+            lineRenderer.positionCount = 5;
+            //Set the positions of the LineRenderer to form a rectangle
+            lineRenderer.SetPosition(0, new Vector3(minX, minY, 0));
+            lineRenderer.SetPosition(1, new Vector3(maxX, minY, 0));
+            lineRenderer.SetPosition(2, new Vector3(maxX, maxY, 0));
+            lineRenderer.SetPosition(3, new Vector3(minX, maxY, 0));
+            lineRenderer.SetPosition(4, new Vector3(minX, minY, 0));
+
+            //Set the color and width of the LineRenderer
+            lineRenderer.startColor = Color.black;
+            lineRenderer.endColor = Color.black;
+            //Set the width of the LineRenderer
+            lineRenderer.startWidth = lineWidth;
+            lineRenderer.endWidth = lineWidth;
+        }
+    }
+}
--- 2DRove/Assets/Scripts/MapGen.cs
+++ 2DRove/Assets/Scripts/MapGen.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using CreateBorder;
 
 public class MapGen : MonoBehaviour
 {
     [SerializeField] Sprite startSprite;
-    [SerializeField] int cycles = 5;
+    [SerializeField] int cycles = 2;
     [SerializeField] int steps = 50;
-    [SerializeField] int tileSizeX = 5;
-    [SerializeField] int tileSizeY = 8;
+    [SerializeField] int tileSizeX = 20;
+    [SerializeField] int tileSizeY = 20;
+    [SerializeField] int maxX = 5;
+    [SerializeField] int maxY = 5;
+    [SerializeField] int minX = -5;
+    [SerializeField] int minY = -5;
 
     void Start()
     {
+        // Check if the number of steps and cycles is too large for the given bounds
+        if(steps*cycles > (maxX+maxY)*(Math.Abs(minX)+Math.Abs(minY)))
+        {
+            Debug.LogError("The number of steps and cycles is too large for the given bounds.");
+            return;
+        }
+        //Create Black Border Line reprenseting the outline boundary
+        DrawBorder drawBorder = new();
+        drawBorder.DrawBorderLine((minX-1)*tileSizeX, (minY-1)*tileSizeY, (maxX+1)*tileSizeX, (maxY+1)*tileSizeY);
         // Dictionary to store positions of tiles
         var tilePositions = new Dictionary<Vector2Int, GameObject>();
     
@@ -35,29 +50,64 @@ public class MapGen : MonoBehaviour
                     go.transform.position = new Vector3(currentPosition.x * tileSizeX, currentPosition.y * tileSizeY, 0);
                     go.AddComponent<SpriteRenderer>().sprite = startSprite;
                     go.transform.localScale = new Vector3(tileSizeX, tileSizeY, 1);
-                    go.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+                    go.GetComponent<SpriteRenderer>().color = new Color(UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f));
                     // Store tile position in the dictionary
                     tilePositions.Add(currentPosition, go);
                 }
 
                 // Randomly update current position
-                switch (Random.Range(0, 4))
+                switch (UnityEngine.Random.Range(0,4))
                 {
                     case 0:
-                        currentPosition.x++;
+                        if (currentPosition.x < maxX) // maxX is the upper bound for x
+                        {
+                            currentPosition.x++;
+                        }
+                        else if(currentPosition.x >= maxX)
+                        {
+                            Debug.Log("Max X reached, moving back");
+                            currentPosition.x--;
+                        }
                         break;
                     case 1:
-                        currentPosition.x--;
+                        if (currentPosition.x > minX) // minX is the lower bound for x
+                        {
+                            currentPosition.x--;
+                        }
+                        else if(currentPosition.x <= minX)
+                        {
+                            Debug.Log("Min X reached, moving forward");
+                            currentPosition.x++;
+                        }
                         break;
                     case 2:
-                        currentPosition.y++;
+                        if (currentPosition.y < maxY) // maxY is the upper bound for y
+                        {
+                            currentPosition.y++;
+                        }
+                        else if(currentPosition.y >= maxY)
+                        {
+                            Debug.Log("Max Y reached, moving down");
+                            currentPosition.y--;
+                        }
                         break;
                     case 3:
-                        currentPosition.y--;
+                        if (currentPosition.y > minY) // minY is the lower bound for y
+                        {
+                            currentPosition.y--;
+                        }
+                        else if(currentPosition.y <= minY)
+                        {
+                            Debug.Log("Min Y reached, moving up");
+                            currentPosition.y++;
+                        }
                         break;
                 }
             }
             Debug.Log("Tiles generated: " + tilePositions.Count);
         }
     }
+
 }
+
+

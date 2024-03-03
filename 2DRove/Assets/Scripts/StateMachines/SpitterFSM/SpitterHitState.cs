2b3387adce2b9bc7ce2b176ee053fec63a9f1ae6
Yordy <yordycruz0329@gmail.com>
2024-03-02 19:52:29 -0800

    Spitter_Enemy (Animations/StateMachine)
    Update to the Spitter enemy, integrating a functional state machine alongside the animations
    
    States: Spawn, Idle,Waliking, Aggro, Attack, Hit and Death.

---
+++ 2DRove/Assets/Scripts/StateMachines/SpitterFSM/SpitterHitState.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SpitterHitState : SpitterBaseState
+{
+    public override void EnterState(SpitterStateManager spitter)
+    {
+
+    }
+
+    public override void UpdateState(SpitterStateManager spitter)
+    {
+
+    }
+
+    public override void OnCollisionEnter2D(SpitterStateManager spitter, Collision2D other)
+    {
+        
+    } 
+    public override void OnTriggerStay2D(SpitterStateManager spitter, Collider2D other) 
+    {
+        
+    }
+}
---
+++ 2DRove/Assets/Scripts/StateMachines/SpitterFSM/SpitterSpawnState.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpitterSpawnState : SpitterBaseState
+{
+    // Start is called before the first frame update
+    private float spawnTime = 1f;
+    public override void EnterState(SpitterStateManager spitter){
+        Debug.Log("Spitter Entering Spawn State...");
+    }
+    public override void UpdateState(SpitterStateManager spitter){
+         if (spawnTime<= 0 ){
+            spitter.SwitchState(spitter.IdleState);
+         }
+         spawnTime -= Time.deltaTime;
+    }
+    public override void OnCollisionEnter2D(SpitterStateManager spitter, Collision2D other){
+    
+    }
+    public override void OnTriggerStay2D(SpitterStateManager spitter, Collider2D other){
+        Debug.Log(other.tag); //
+        if(other.tag == "Player"){
+            spitter.SwitchState(spitter.AttackState);
+        }
+    }
+}

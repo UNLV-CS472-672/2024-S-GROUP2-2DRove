7f043ce7fc446f08f133c654d254bd85bc4b4ad0
SwiftNemesis <bbsowers@gmail.com>
2024-03-21 20:36:48 -0700

    Removed a compilation error.

--- 2DRove/Assets/Scripts/Player.cs
+++ 2DRove/Assets/Scripts/Player.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using MapGenDLA;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 

9bfa245aeb5edfed309832982d82b1d82b888a3f
Shijie Lin <1369862453@qq.com>
2024-03-22 20:29:46 -0700

    Add comments for OptionsMenu.cs

--- 2DRove/Assets/Scripts/OptionsMenu.cs
+++ 2DRove/Assets/Scripts/OptionsMenu.cs
@@ -8,11 +8,11 @@ using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;   
+    public AudioMixer audioMixer;               // Audio mixer to control the volume of the game
 
-    public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown resolutionDropdown;     // Dropdown to select the resolution of the game
 
-    Resolution[] resolutions;
+    Resolution[] resolutions;                   // Array to store all the resolutions that the screen supports
 
     void Start(){
         // Set the resolution dropdown to the current resolution
@@ -30,15 +30,17 @@ public class OptionsMenu : MonoBehaviour
         Screen.fullScreen = isFullscreen;
     }
   
+    // Set the screen resolution of the game
     public void setResolution(){
-        resolutions = Screen.resolutions;
+        resolutions = Screen.resolutions;           // Get all the resolutions that the screen supports
 
-        resolutionDropdown.ClearOptions();
+        resolutionDropdown.ClearOptions();          // Clear the current options in the dropdown
 
-        List<string> options = new List<string>();
+        List<string> options = new List<string>();  // Create a list of options for the dropdown
 
-        int currentResolutionIndex = 0;
+        int currentResolutionIndex = 0;             // Index of the current resolution
 
+        // Loop through all the resolutions and add them to the options list
         for(int i = 0; i < resolutions.Length; i++){
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
@@ -49,6 +51,7 @@ public class OptionsMenu : MonoBehaviour
             }
         }
 
+        // Add the options to the dropdown and set the current resolution
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();

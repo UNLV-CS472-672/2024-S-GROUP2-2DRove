7956737bb6ef62c5791d0ed772fb283368b7b5e4
Shijie Lin <1369862453@qq.com>
2024-03-23 17:20:03 -0700

    Update Player's UI
    - Create Minimap.cs
    - Add Minimap Feature
    - Add Zoom In/Out Feature to Minimap

---
+++ 2DRove/Assets/Scripts/Minimap.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{   
+    private Camera MinimapCamera;       // Reference to the minimap camera
+
+    private float MinimapScale;         // Variable to store the minimap zoom
+
+    // Start is called before the first frame update
+    private void Start(){
+        MinimapCamera = GameObject.Find("MiniMap/Camera").GetComponent<Camera>();       // Get the minimap camera component
+        MinimapScale = MinimapCamera.orthographicSize;                                  // Get the initial zoom of the minimap
+    }                            
+
+    // Function to zoom in the minimap
+    public void MinimapZoomIn()
+    {
+        if(MinimapScale > 6){
+            MinimapScale -= 3;    // Decaease the scale of the minimap by 3
+            MinimapCamera.orthographicSize = MinimapScale;
+        }
+    }
+
+    // Function to zoom out the minimap
+    public void MinimapZoomOut()
+    {
+        if(MinimapScale < 20){
+            MinimapScale += 3;    // Increase the scale of the minimap by 3
+            MinimapCamera.orthographicSize = MinimapScale;
+        }
+    }
+
+}

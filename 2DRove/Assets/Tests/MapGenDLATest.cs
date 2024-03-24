66fb21d7729d7cc3ca3e11ca8529bd9d4c0528cd
tnishamon <92564751+tnishamon@users.noreply.github.com>
2024-03-24 14:15:56 -0700

    Test Pipeline has been implemented
    We can now probably work on merging this into mapgen to have remote testing and code coverage

--- 2DRove/Assets/Tests/MapGenDLATest.cs
+++ 2DRove/Assets/Tests/MapGenDLATest.cs
@@ -20,20 +20,40 @@ public class MapGenDLATest
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator MapGenerated()
+    public IEnumerator LoadProperMap()
     {
-        yield return SceneManager.LoadSceneAsync("Maps/Floor1", LoadSceneMode.Single);
-
+        yield return SceneManager.LoadSceneAsync("Tests/Test1");
 
         //GameObject mapGenObject = GameObject.Find("Start");
         //MapGenDLA.MapGenDLA mapGenerator = mapGenObject.GetComponent<MapGenDLA.MapGenDLA>();
         //Assert.IsNotNull(mapGenerator, "MapGenDLA component not found");
 
+        yield return null;
 
+        yield return SceneManager.UnloadSceneAsync("Tests/Test1");
+
+        
+
+    }
+    [UnityTest]
+    public IEnumerator LoadImproperMap()
+    {
+        yield return SceneManager.LoadSceneAsync("Tests/Test2");
 
         yield return null;
 
+        yield return SceneManager.UnloadSceneAsync("Tests/Test2");
 
+        yield return SceneManager.LoadSceneAsync("Tests/Test3");
+
+        yield return null;
+
+        yield return SceneManager.UnloadSceneAsync("Tests/Test3");
+
+        yield return SceneManager.LoadSceneAsync("Tests/Test4");
+
+        yield return null;
+
+        yield return SceneManager.UnloadSceneAsync("Tests/Test4");
     }
-
 }

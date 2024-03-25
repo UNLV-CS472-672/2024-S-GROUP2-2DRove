77de2535f187cda6892e441e041287803f877b4d
tnishamon <92564751+tnishamon@users.noreply.github.com>
2024-03-24 21:07:24 -0700

    Revert "Merge branch 'mapgen' into testtest"
    This reverts commit cc4257a519f3d10813cdecefb9bbe9de492c064f, reversing
    changes made to 18e65dd34e3f25966a0a511079fa857d955cfbc2.

--- 2DRove/Assets/Scripts/MapGenDLA.cs
+++ 2DRove/Assets/Scripts/MapGenDLA.cs
@@ -1,19 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
-// using CreateBorder;
-using UnityEngine.Tilemaps;
-using UnityEngine.SceneManagement;
 
-namespace MapGenDLANamespace
+namespace MapGenDLA
 {
     public class MapGenDLA : MonoBehaviour
     {
-
-        [SerializeField] GameObject tile;
-        [SerializeField] GameObject borderPrefab;
-
-        [SerializeField] GameObject exitPrefab;
-
+        [SerializeField] public GameObject tile;
         // [SerializeField] int cycles = 2;
         // [SerializeField] int steps = 50;
         
@@ -21,25 +13,18 @@ namespace MapGenDLANamespace
         // [SerializeField] int tileSizeX = 20;
         // [SerializeField] int tileSizeY = 20;
 
-        [SerializeField] public int scale = 20;
-        [SerializeField] public int maxX = 5;
-        [SerializeField] public int maxY = 5;
-        [SerializeField] public int minX = -5;
-        [SerializeField] public int minY = -5;
+        [SerializeField] int scale = 20;
+        [SerializeField] int maxX = 5;
+        [SerializeField] int maxY = 5;
+        [SerializeField] int minX = -5;
+        [SerializeField] int minY = -5;
         // Dictionary to store positions of tiles
         // public static Dictionary<Vector2Int, GameObject> tilePositions = new();
         //enum Direction { UpRight, DownLeft, UpLeft, DownRight };
         public static HashSet<Vector2Int> tilePositions = new();
-        // We need to make sure we are not creating border tiles on top of each other
-        public static HashSet<Vector2Int> borderPositions = new();
-        public static Dictionary<Vector2Int, GameObject> tileObjects = new();
 
         void Start()
         {
-            //Clear at the start of a scene being loaded
-            tilePositions.Clear();
-            borderPositions.Clear();
-            tileObjects.Clear();
             //checks for SerializeFields
             int possibleTiles = Mathf.Abs(maxX-minX+1) * Mathf.Abs(maxY-minY+1);
             if(possibleTiles < tileNum){
@@ -62,12 +47,6 @@ namespace MapGenDLANamespace
             // Generate first tile at (0, 0)
             GenerateFirstTile(currentPosition);
 
-            // Used for determining endpoint direction for implementing map generation direction bias
-            Vector2Int endpoint = GenerateRandomBorderEndpoint();
-
-            // Generate Endpoint tile 
-            //CreatePreviousTile(endpoint);
-
             int i = 1;  // Tile counter 
             while(i < tileNum){
 
@@ -75,41 +54,15 @@ namespace MapGenDLANamespace
                 currentPosition = RandomDirection();
 
                 // Find open, non-border tile 
-                FindOpenTile(ref currentPosition, ref previousPosition, endpoint);
+                FindOpenTile(ref currentPosition, ref previousPosition);
 
                 //Create a new tile where the previous tile was
                 CreatePreviousTile(previousPosition);
                 i++;
             }
 
-
-            foreach (var tile in tileObjects)
-            {
-                Vector2Int position = tile.Key;
-                GameObject tileObject = tile.Value;
-                // Our directions considering we are isometric
-                Vector2Int RightUp = Vector2Int.right + Vector2Int.up;
-                Vector2Int LeftUp = Vector2Int.left + Vector2Int.up;
-                Vector2Int RightDown = Vector2Int.right + Vector2Int.down;
-                Vector2Int LeftDown = Vector2Int.left + Vector2Int.down;
-
-                //Check the neighboring tiles
-                CheckAndAddBorder(position + RightUp, tileObject);
-                CheckAndAddBorder(position + LeftUp, tileObject);
-                CheckAndAddBorder(position + RightDown, tileObject);
-                CheckAndAddBorder(position + LeftDown, tileObject);
-            }
-
-            // //Add an exit
-            // Vector2Int farthestTile = FindFarthestTile(new Vector2Int(0, 0));
-            // AddExit(tileObjects[farthestTile], farthestTile);
-            Vector2Int closestTile = FindClosestGeneratedTileToEndpoint(endpoint);
-            AddExit(closestTile);
-
-
-
-            // //todo: implement FillInEmptySpace to work for isometric
-            // // FillInEmptySpace();
+            //todo: implement FillInEmptySpace to work for isometric
+            // FillInEmptySpace();
 
         }
 
@@ -118,10 +71,8 @@ namespace MapGenDLANamespace
         {
             GameObject firstTile = Instantiate(tile, new Vector3(0, 0, 0), Quaternion.identity);
             firstTile.name = "Tile(0,0)";
-            //firstTile.layer = LayerMask.NameToLayer("TileLayer");
             firstTile.transform.localScale = new Vector3(scale, scale, 1);
             tilePositions.Add(new Vector2Int(0,0));
-            tileObjects.Add(new Vector2Int(0,0), firstTile);
 
             Debug.Log("Starting Tile Made!");
         }
@@ -141,7 +92,7 @@ namespace MapGenDLANamespace
             return borderDirections[startSide];  // Return the selected border position
         }
 
-        private void FindOpenTile(ref Vector2Int currentPosition, ref Vector2Int previousPosition, Vector2Int targetPosition)
+        private void FindOpenTile(ref Vector2Int currentPosition, ref Vector2Int previousPosition)
         {
             // if the any of the start sides already has a tile on top of it
             // technically this isnt DLA but code breaks if a tile lands on the border of the map
@@ -151,7 +102,7 @@ namespace MapGenDLANamespace
 
                     //do random walks to place the next tile close by
                     int direction = Random.Range(0, 4);
-                    currentPosition = UpdatePosition(direction, currentPosition, targetPosition);
+                    currentPosition = UpdatePosition(direction, currentPosition);
                 }
                 previousPosition = currentPosition;
             }
@@ -168,191 +119,62 @@ namespace MapGenDLANamespace
 
                     // Update current position based on random direction
                     int direction = Random.Range(0, 4);
-                    currentPosition = UpdatePosition(direction, currentPosition, targetPosition);
+                    currentPosition = UpdatePosition(direction, currentPosition);
                 }
             }
         }
 
-        private Vector2Int UpdatePosition(int direction, Vector2Int position, Vector2Int targetPosition)
+        private Vector2Int UpdatePosition(int direction, Vector2Int position)
         {
-            Vector2Int biasDirection = GetBiasDirection(position, targetPosition);
-            int bias = Random.Range(0, 10); // Introduce a bias probability, e.g., 20% chance to follow the bias direction
-
-            if (bias < 2) // 20% chance to move towards the target
+            /*
+                Mathf.Min returns the smaller of the two numbers
+                Mathf.Max returns the larger of the two numbers
+                Example: Mathf.Min(5, 10) returns 5
+            */
+            switch (direction)
             {
-                return new Vector2Int(
-                    Mathf.Clamp(position.x + biasDirection.x, minX, maxX),
-                    Mathf.Clamp(position.y + biasDirection.y, minY, maxY)
-                );
+                case 0: return new Vector2Int(Mathf.Clamp(position.x + 1,minX, maxX), Mathf.Clamp(position.y+1, minY, maxY));//ur
+                case 1: return new Vector2Int(Mathf.Clamp(position.x - 1, minX, maxX), Mathf.Clamp(position.y-1, minY, maxY));//dl
+                case 2: return new Vector2Int(Mathf.Clamp(position.x-1, minX, maxX), Mathf.Clamp(position.y + 1, minY, maxY));//ul
+                case 3: return new Vector2Int(Mathf.Clamp(position.x+1, minX, maxX), Mathf.Clamp(position.y - 1, minY, maxY));//dr
+
+                default: return position;
             }
-            else
-            {
-                /*
-                    Mathf.Min returns the smaller of the two numbers
-                    Mathf.Max returns the larger of the two numbers
-                    Example: Mathf.Min(5, 10) returns 5
-                */
-                int distance = 1;
-                switch (direction)
-                {
-                    case 0: return new Vector2Int(Mathf.Clamp(position.x + distance,minX, maxX), Mathf.Clamp(position.y + distance, minY, maxY));//ur
-                    case 1: return new Vector2Int(Mathf.Clamp(position.x - distance, minX, maxX), Mathf.Clamp(position.y - distance, minY, maxY));//dl
-                    case 2: return new Vector2Int(Mathf.Clamp(position.x - distance, minX, maxX), Mathf.Clamp(position.y + distance, minY, maxY));//ul
-                    case 3: return new Vector2Int(Mathf.Clamp(position.x + distance, minX, maxX), Mathf.Clamp(position.y - distance, minY, maxY));//dr
-
-                    default: return position;
-                }
-            }
-
-        }
-
-        private Vector2Int GetBiasDirection(Vector2Int currentPosition, Vector2Int targetPosition)
-        {
-            // Calculate the direction vector towards the target
-            Vector2Int direction = targetPosition - currentPosition;
-            direction.x = (int)Mathf.Sign(direction.x);
-            direction.y = (int)Mathf.Sign(direction.y);
-            return direction;
         }
 
         //Create a new tile where the previous tile was
         private void CreatePreviousTile(Vector2Int previousPosition)
         {
             GameObject newTile = Instantiate(tile, new Vector3(previousPosition.x * scale * 10, previousPosition.y * scale * 5, 0), Quaternion.identity);
-            //newTile.layer = LayerMask.NameToLayer("TileLayer");
             newTile.name = "Tile(" + previousPosition.x + ", " + previousPosition.y + ")";
             newTile.transform.localScale = new Vector3(scale, scale, 1);
             tilePositions.Add(previousPosition);
-
-            tileObjects.Add(previousPosition, newTile);
+            Debug.Log("Tile(" + previousPosition.x + ", " + previousPosition.y + ") Made!");
         }
-
-
-        // Deprecated for now.
+// Deprecated, we implemented a new method to do this on another branch
+/*
         // Fills in the empty space created between tiles and the border similarly to tile generation. 
-        // void FillInEmptySpace()
-        // {
-        //     // Generate empty space
-        //     for (int i = minX-1; i <= maxX+1; i++)
-        //         for (int j = minY-1; j <= maxY+1; j++)
-        //         {
-        //             Debug.Log("Generating empty space for: " + i + " " + j);
-        //             // Check if the position is already occupied
-        //             if (!tilePositions.Contains(new Vector2Int(i, j)))
-        //             {
-        //                 GameObject emptyTiles = Instantiate(tile, new Vector3(i * scale*10, j * scale*5, 0), Quaternion.identity);
-        //                 emptyTiles.name = "EmptyTile(" + i + ", " + j + ")";
-        //                 emptyTiles.transform.localScale = new Vector3(scale, scale, 1);
-        //                 emptyTiles.GetComponent<SpriteRenderer>().color = Color.black;
-        //                 emptyTiles.AddComponent<BoxCollider2D>();
-        //                 tilePositions.Add(new Vector2Int(i, j));
-        //             }
-        //         }
-        //     Debug.Log("Empty Space generated: " + tilePositions.Count);
-        // }
-
-        void CheckAndAddBorder(Vector2Int position, GameObject tileObject)
+        void FillInEmptySpace()
         {
-            // If the neighboring tile is empty (either a basic tile or border), add a border
-            if (!tilePositions.Contains(position) && !borderPositions.Contains(position))
-            {
-                AddBorder(tileObject, position);
-            }
-        }
-        void AddBorder(GameObject tileObject, Vector2Int position)
-        {
-            GameObject border = Instantiate(borderPrefab, new Vector3(position.x * scale * maxX, position.y * scale * maxY/2, 0), Quaternion.identity);
-            border.name = "Border(" + position.x + ", " + position.y + ")";
-            border.transform.parent = tileObject.transform;
-            border.transform.localScale = new Vector3(scale / 2, scale / 2, 1);
-            // Place the tile lower in the layers
-            border.GetComponent<Renderer>().sortingOrder = -1;
-            // Apply a tilemap collider to give all the tiles on the tilemap a collider
-            Tilemap tilemap = border.GetComponent<Tilemap>();
-            TilemapCollider2D collider = border.AddComponent<TilemapCollider2D>();
-            // Keep it in a list so we do not stack borders on top of each other when checking
-            borderPositions.Add(position);
-        }
-
-        void AddExit(Vector2Int closestTile)
-        {
-            if (tileObjects.TryGetValue(closestTile, out GameObject tileObject))
-            {
-                // Use the tileObject's position, as it might have been adjusted for your game world
-                Vector3 exitPosition = tileObject.transform.position + new Vector3(0, 0, -1); // Adjust Z if necessary to ensure visibility
-                GameObject exit = Instantiate(exitPrefab, exitPosition, Quaternion.identity);
-                exit.name = "Exit(" + closestTile.x + ", " + closestTile.y + ")";
-                exit.transform.localScale = new Vector3(scale, scale, 1);
-                BoxCollider2D collider = exit.AddComponent<BoxCollider2D>();
-                collider.isTrigger = true;
-            }
-            else
-            {
-                Debug.LogError("Closest tile to endpoint not found in tileObjects.");
-            }
-        }
-
-
-        Vector2Int FindClosestGeneratedTileToEndpoint(Vector2Int endpoint)
-        {
-            Vector2Int closestTile = new Vector2Int(0, 0);
-            float minDistance = float.MaxValue; // Corrected initialization
-            foreach (Vector2Int tilePosition in tilePositions)
-            {
-                float distance = Vector2.Distance(tilePosition, endpoint);
-                if (distance < minDistance)
+            // Generate empty space
+            for (int i = minX-1; i <= maxX+1; i++)
+                for (int j = minY-1; j <= maxY+1; j++)
                 {
-                    minDistance = distance;
-                    closestTile = tilePosition;
+                    Debug.Log("Generating empty space for: " + i + " " + j);
+                    // Check if the position is already occupied
+                    if (!tilePositions.Contains(new Vector2Int(i, j)))
+                    {
+                        GameObject emptyTiles = Instantiate(tile, new Vector3(i * scale*10, j * scale*5, 0), Quaternion.identity);
+                        emptyTiles.name = "EmptyTile(" + i + ", " + j + ")";
+                        emptyTiles.transform.localScale = new Vector3(scale, scale, 1);
+                        emptyTiles.GetComponent<SpriteRenderer>().color = Color.black;
+                        emptyTiles.AddComponent<BoxCollider2D>();
+                        tilePositions.Add(new Vector2Int(i, j));
+                    }
                 }
-            }
-            return closestTile;
+            Debug.Log("Empty Space generated: " + tilePositions.Count);
         }
-
-
-
-
-        Vector2Int FindFarthestTile(Vector2Int startPoint)
-        {
-            Vector2Int farthestTile = new Vector2Int(0, 0);
-            float maxDistance = 0;
-            foreach (var tile in tilePositions)
-            {
-                float distance = Vector2Int.Distance(startPoint, tile);
-                if (distance > maxDistance)
-                {
-                    maxDistance = distance;
-                    farthestTile = tile;
-                }
-            }
-            return farthestTile;
-        }
-
-
-        Vector2Int GenerateRandomBorderEndpoint()
-        {
-            int side = Random.Range(0, 4); // Randomly select one of the four sides
-            int randomPosition;
-
-            switch (side)
-            {
-                case 0: // Top
-                    randomPosition = Random.Range(minX, maxX + 1);
-                    return new Vector2Int(randomPosition, maxY);
-                case 1: // Bottom
-                    randomPosition = Random.Range(minX, maxX + 1);
-                    return new Vector2Int(randomPosition, minY);
-                case 2: // Left
-                    randomPosition = Random.Range(minY, maxY + 1);
-                    return new Vector2Int(minX, randomPosition);
-                case 3: // Right
-                    randomPosition = Random.Range(minY, maxY + 1);
-                    return new Vector2Int(maxX, randomPosition);
-                default:
-                    return new Vector2Int(0, 0); // This case should never be reached
-            }
-        }
-
+*/
     }
 
 }

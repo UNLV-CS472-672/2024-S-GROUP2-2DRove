bacaecb9676307c27dce09df02c727a7db58d5ab
SwiftNemesis <bbsowers@gmail.com>
2024-04-09 00:27:57 -0700

    Added a lot stuff to make augments work idk. Created prefabs as well for UI.

---
+++ 2DRove/Assets/Scripts/SelectedAugmentList.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectedAugmentList : MonoBehaviour
+{
+    public Button closeAugmentList;
+    public Button augmentList;
+    public Canvas augmentListOverlay;
+    public GameObject augmentPrefab;
+    public Transform augmentListTransform;
+    void Start()
+    {
+        closeAugmentList.onClick.AddListener(CloseAugmentList);
+        augmentList.onClick.AddListener(ShowAugmentList);
+    }
+
+    void ShowAugmentList()
+    {
+        pause();
+        foreach(Transform child in augmentListTransform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach(string augment in Augments.chosenAugments)
+        {
+            GameObject augmentCard = Instantiate(augmentPrefab, augmentListTransform);
+            augmentCard.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = augment;
+        }
+        augmentListOverlay.gameObject.SetActive(true);
+    }
+    void CloseAugmentList()
+    {
+        unpause();
+        augmentListOverlay.gameObject.SetActive(false);
+    }
+
+    void pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    void unpause()
+    {
+        Time.timeScale = 1;
+    }
+}

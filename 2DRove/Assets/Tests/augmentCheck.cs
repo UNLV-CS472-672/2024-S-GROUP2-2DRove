efbee4af55e87550910024f280e66a7b27ac9faa
tnishamon <92564751+tnishamon@users.noreply.github.com>
2024-04-23 22:42:16 -0700

    Cleaning up and preparing a new test

---
+++ 2DRove/Assets/Tests/augmentCheck.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class augmentCheck
+{
+    [UnityTest]
+    IEnumerator InitializeAugments()
+    {
+        GameObject augment = new GameObject();
+    }
+}
